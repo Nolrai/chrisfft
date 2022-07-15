3835ee0ee57f95bf2d079127549cc4d4ef3819a1
Christina A. Upshaw <chrisaupshaw@gmail.com>
2022-07-15 04:44:50 -0700

    No tests yet.

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,4 +1,57 @@
-import Lib (inc)
+import System.Process
+import Data.Vector.Unboxed as V
+import GHC.Float (sqrtDouble)
+import GHC.IO.Handle.Text (hPutStrLn)
 
+mkWave :: Double -> Double -> Int -> Complex Double
+mkWave freq deltaX ix = let t = deltaX * fromIntegral ix in cis (2 pi * freq * t)
+
+mkSignal ix = wave (60) 0.01 ix + wave (100) 0.01 ix
+
+signal deltaX length = generate (floor (length / deltaX)) mkSignal
+
+noisePoint :: IO (Complex Double)
+noisePoint = do
+    x <- normalIO' 0 (sqrtDouble 2)
+    y <- normalIO' 0 (sqrtDouble 2)
+    pure (x :+ y)
+
+whiteNoise numPoints = V.replicateM numPoints noisePoint
+
+brownNoise :: Double -> Double -> IO (Vector (Complex Double))
+brownNoise deltaX length = do
+    let numPoints = floor (length / deltaX)
+    rawBrown <- scanl1 (+) whiteNoise numPoints
+    let maxValue = foldl' (\ a b -> max a (abs b)) rawBrown
+    ((length / maxValue) * ) <$> rawBrown -- scale it down to a total drift of 1
+
+noise deltaX length = zipWith (+) <$> whiteNoise <*> brownNoise
+
+noisySignal deltaX length = zipWith (+) (signal deltaX length) <$> noise deltaX length
+
+time deltaX length = generate (floor (length / deltaX)) (\ ix -> ix * deltaX)
+ 
 main :: IO ()
-main = print . inc $ (41 :: Int)
+main = do
+    
+    let time' = time deltaT length
+    let signal' = signal deltaT length
+    noisySignal' <- noisySignal deltaT length
+    let transformed = fft noisySignal'
+    let maxPower = foldl' (\ a b -> max a (abs b)) transformed
+    let clippedFft = (\ x -> if abs x > (maxPower / 2) then x else 0) `V.map` maxPower
+    let reconstructed = invFft clippedFft
+    let errorV = (\ a b -> magnitude (a - b)) `V.zipWith` signal' reconstructed
+    
+    timeData <- openFile WriteMode "timeData.txt"
+    hPutStrLn timeData "T Signal NoisySignal Reconstructed Error"
+    hPutStrLn dataFile <$> V.zipWith5 formatVectors5 time' signal' noisySignal' reconstructed errorV
+    hClose timeData
+
+    freqData <- openFile WriteMode "timeData.txt"
+    hPutStrLn freqData "W transformed"
+    hPutStrLn freqData <$> V.zipWith5 formatVectors4 time' transformed clipped
+    hClose freqData
+
+    callProcess "gnuplot" ["-s", "-p", "simple.1.gnu"]
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,44 @@
+{-# LANGUAGE TupleSections  #-}
+
 -- | Example of a library file. It is also used for testing the test suites.
 module Lib
   (
     -- * Exported functions
-    ditfft2
+    fft,
+    invFft
   ) where
 
-import Data.Vector.Unboxed
+import Data.Vector.Unboxed as V
+    ( (!), drop, generate, imap, length, map, Vector )
+import Data.Complex ( cis, Complex )
 
-ditfft2 x 0 s = x
-ditfft2 x twoPow s =
-  
+data ForwardOrInverse = Forward | Inverse
+
+fft :: Vector (Complex Double) -> Vector (Complex Double)
+fft v = ditfft2 Forward v (V.length v) 1
+invFft :: Vector (Complex Double) -> Vector (Complex Double)
+invFft v = (/ fromIntegral (V.length v)) `V.map` ditfft2 Inverse v (V.length v) 1
+
+ditfft2 ::
+  ForwardOrInverse ->
+  Vector (Complex Double) ->
+  Int ->
+  Int ->
+    Vector (Complex Double)
+ditfft2 _ v 1 _ = v
+ditfft2 b v fullSize stride =
+  generate fullSize $ \ ix -> if ix < halfSize then (p ! ix) + (q ! ix) else (p ! ix) - (q ! ix)
+  where
+  p, q :: Vector (Complex Double)
+  p = next v 
+  q = (\ ix -> ( wPow ix * )) `imap` next (V.drop 1 v)
+  wPow :: Int -> Complex Double
+  wPow n = cis $ sign * 2 * pi * fromIntegral n / fromIntegral fullSize
+  next :: Vector (Complex Double) -> Vector (Complex Double)
+  next v' = ditfft2 b v' halfSize (2 * stride)
+  halfSize = fullSize `div` 2
+  sign :: Double
+  sign =
+    case b of
+    Forward -> 1
+    Inverse -> -1

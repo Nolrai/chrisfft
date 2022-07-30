ede74a2c680f5d219c1fb1c2251059e91d5fa34a
Christina A. Upshaw <chrisaupshaw@gmail.com>
2022-07-29 18:59:51 -0700

    WIP

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -2,54 +2,41 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-import Data.Complex (Complex (..), cis, magnitude, polar)
+import Data.Complex (Complex (..), cis, magnitude)
 import Data.Random.Normal (normalIO')
 import Data.Vector as V
   ( Vector,
     foldl',
     freeze,
+    filter,
     generate,
+    length,
     map,
     mapM_,
     replicateM,
     scanl1,
-    take,
+    sum,
     thaw,
     zip,
     zipWith,
     zipWith3,
-    zipWith5,
+    zipWith5, forM
   )
-import Data.Vector.Algorithms.Intro as A (partialSortBy)
+import qualified Data.Vector.Mutable as VM (take)
+import Data.Vector.Algorithms.Intro as A (partialSortBy, sortBy)
+import FFT (fft, invFft)
 import GHC.Float (sqrtDouble)
-import GHC.Stack ( HasCallStack )
-import Lib (fft, invFft)
 import Relude
-  ( Applicative (pure, (<*>)),
-    Double,
-    Floating (pi),
-    Fractional ((/)),
-    IO,
-    Int,
-    Num ((*), (+), (-)),
-    Ord (compare, max, (>)),
-    RealFloat,
-    RealFrac (floor),
-    Semigroup ((<>)),
-    Text,
-    appendFileText,
-    fromIntegral,
-    on,
-    print,
-    putTextLn,
-    show,
-    snd,
-    writeFileText,
-    ($),
-    (.),
-    (<$>),
-  )
-import System.Process (callProcess)
+-- import System.Process (callProcess)
+
+meanSquare :: Vector (Complex Double) -> Double
+meanSquare = mean . V.map magnitude
+  where
+    mean :: Vector Double -> Double
+    mean u = V.sum u / fromIntegral (V.length u)
+
+rootMeanSquare :: Vector (Complex Double) -> Double
+rootMeanSquare = sqrtDouble . meanSquare
 
 mkWave :: HasCallStack => Double -> Double -> Int -> Complex Double
 mkWave freq deltaX ix = let t = deltaX * fromIntegral ix in cis (2 * pi * freq * t :: HasCallStack => Double)
@@ -58,7 +45,7 @@ mkSignal :: HasCallStack => Int -> Complex Double
 mkSignal ix = mkWave 60 0.01 ix + mkWave 100 0.01 ix
 
 signal :: HasCallStack => Double -> Double -> Vector (Complex Double)
-signal deltaX duration = V.generate (floor (duration / deltaX)) mkSignal
+signal deltaX duration = V.generate ((2 :: Int) ^ (floor (logBase 2 (duration / deltaX)) :: Int)) mkSignal
 
 noisePoint :: HasCallStack => IO (Complex Double)
 noisePoint = do
@@ -88,31 +75,55 @@ noisySignal deltaX duration = V.zipWith (+) (signal deltaX duration) <$> noise d
 time :: HasCallStack => Double -> Double -> Vector Double
 time deltaX duration = generate (floor (duration / deltaX)) (\ix -> fromIntegral ix * deltaX)
 
+putInfoLn :: Text -> Vector (Complex Double) -> IO ()
+putInfoLn name v = do
+  putTextLn $ name <> " length : " <> show (V.length v)
+  putTextLn $ name <> " power : " <> show (meanSquare v)
+  putTextLn $ name <> " rms : " <> show (rootMeanSquare v)
+
+-- flipped so we get _decending_ order when we sort
+byPower :: (Double, Double) -> (Double, Double) -> Ordering
+byPower = flip compare `on` snd
+
 main :: HasCallStack => IO ()
 main = do
   (noisySignal' :: Vector (Complex Double)) <- noisySignal deltaT duration
+  putInfoLn "noisySignal" noisySignal'
   let transformed = fft noisySignal'
+  putInfoLn "transformed" transformed
   let (maxPower :: Double) = maximumMagnitude transformed
+  putTextLn $ "maxPower : " <> show (V.length transformed)
   let (clipped :: Vector (Complex Double)) = (\x -> if magnitude x > (maxPower / 2) then x else 0) `V.map` transformed
+  putTextLn $ "number uncliped : " <> show (V.length . V.filter (\ x -> magnitude x > maxPower / 2) $ clipped)
+  putInfoLn "clipped" clipped
   let reconstructed = invFft clipped
-  let errorV = V.zipWith (\a b -> magnitude (a - b)) signal' reconstructed
-
+  putInfoLn "reconstructed" reconstructed
+  let errorV = V.zipWith (-) signal' reconstructed
+  putInfoLn "errorV" errorV
+  let errorMagnitude = V.map magnitude errorV
+  
   let timeData = "timeData.txt"
   writeFileText timeData "T Signal NoisySignal Reconstructed Error \n"
-  appendFileText timeData `V.mapM_` V.zipWith5 toSpacedStrings5 time' signal' noisySignal' reconstructed errorV
+  appendFileText timeData `V.mapM_` V.zipWith5 toSpacedStrings5 time' signal' noisySignal' reconstructed errorMagnitude
 
   let freqData = "freqData.txt"
   writeFileText freqData "W transformed clipped \n"
   appendFileText freqData `V.mapM_` V.zipWith3 toSpacedStrings3 time' transformed clipped
 
-  v <- V.thaw . V.zip time' $ V.map polar transformed
-  A.partialSortBy (compare `on` (snd . snd)) v 10
-  top <- V.freeze v
+  v <- V.thaw . V.zip time' $ V.map magnitude transformed
+  A.partialSortBy byPower v 10
+  let top' = VM.take 10 v
+  A.sortBy byPower top'
+  top <- V.freeze top'
 
+  putTextLn ""
   putTextLn "Top 10 frequencies and powers"
-  print $ V.take 10 top
+  _ <- top `V.forM` \ (freq, power) -> 
+    putStrLn $ "freq: " <> show freq <> " Power: " <> show power
 
-  callProcess "gnuplot" ["-s", "-p", "simple.1.gnu"]
+  exitSuccess
+
+  -- callProcess "gnuplot" ["-s", "-p", "simple.1.gnu"]
   where
     deltaT, duration :: HasCallStack => Double
     deltaT = 0.000025
--- src/FFT.hs
+++ src/FFT.hs
@@ -1,16 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables, NoImplicitPrelude, OverloadedStrings #-}
 
 -- | Example of a library file. It is also used for testing the test suites.
-module Lib
+module FFT
   (
     -- * Exported functions
     fft,
-    invFft
+    invFft,
+    cleanComplex,
+    cleanDouble
   ) where
 
 import Data.Vector as V
     -- ( (!), drop, generate, imap, length, map, Vector )
-import Data.Complex ( cis, Complex )
+import Data.Complex ( cis, Complex (..) )
 import Relude
     -- ( ($),
     --   fromIntegral,
@@ -46,11 +48,11 @@ ditfft2 :: HasCallStack =>
     Vector (Complex Double)
 ditfft2 _ v 1 _ = v
 ditfft2 b v fullSize stride =
-  generate fullSize $ \ ix -> if ix < halfSize then p!ix + q!ix else p!(ix - halfSize) - q!(ix - halfSize)
+  generate fullSize $ \ ix -> cleanComplex (if ix < halfSize then p!ix + q!ix else p!(ix - halfSize) - q!(ix - halfSize))
   where
   p, q :: Vector (Complex Double)
   p = next v 
-  q = (\ ix -> ( wPow ix * )) `imap` next (V.drop 1 v)
+  q = (\ ix -> ( wPow ix * )) `imap` next (V.drop stride v)
   wPow :: Int -> Complex Double
   wPow n = cis $ sign * 2 * pi * fromIntegral n / fromIntegral fullSize
   next :: Vector (Complex Double) -> Vector (Complex Double)
@@ -62,3 +64,11 @@ ditfft2 b v fullSize stride =
     Forward -> 1
     Inverse -> -1
 
+cleanComplex :: Floating a => Complex a -> Complex a
+cleanComplex (a :+ b) = cleanDouble a :+ cleanDouble b
+
+cleanDouble :: Floating a => a -> a
+cleanDouble d = (big + d) - big
+  where
+    -- we are just trying to round off the last couple of bits of persision.
+    big = 8
